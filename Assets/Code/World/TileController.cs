fc24142f09139bab16458e27476cb7b9dfbabade
Gregor Mubi <gregor.mubi@outfit7.com>
2019-09-09 15:09:52 +0200

    made city tile prefabs

---
+++ Assets/Code/World/TileController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+    [SerializeField] private SpriteRenderer SpriteRenderer = null;
+
+    [SerializeField] public Sprite SpriteNorth = null;
+    [SerializeField] public Sprite SpriteEast = null;
+    [SerializeField] public Sprite SpriteSouth = null;
+    [SerializeField] public Sprite SpriteWest = null;
+
+    private float Rotation = 0.0f;
+
+    public void SetRotation(float rotation) {
+        while (rotation < 0) {
+            rotation += 360;
+        }
+        while (rotation > 360) {
+            rotation -= 360;
+        }
+
+        Rotation = rotation;
+        if (Rotation < 45) {
+            SpriteRenderer.sprite = SpriteNorth;
+        } else if (Rotation < 135) {
+            SpriteRenderer.sprite = SpriteEast;
+        } else if (Rotation < 225) {
+            SpriteRenderer.sprite = SpriteSouth;
+        } else {
+            SpriteRenderer.sprite = SpriteWest;
+        }
+    }
+}
