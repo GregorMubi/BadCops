eb858603520af7abfe11b38e1346961e0b04dafc
Gregor Mubi <gregor.mubi@outfit7.com>
2019-09-12 14:38:22 +0200

    scenes to buildSettings

--- Assets/Code/MainMenu/MainMenuController.cs
+++ Assets/Code/MainMenu/MainMenuController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
 
     public void Play() {
-        //TODO
+        SceneManager.LoadScene("Game");
     }
 
     public void Exit() {
