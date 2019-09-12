588711654aead18df2179d376e1493854a5156ab
Gregor Mubi <gregor.mubi@outfit7.com>
2019-09-12 14:08:58 +0200

    Added Main menu

---
+++ Assets/Code/MainMenu/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    public void Play() {
+        //TODO
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
