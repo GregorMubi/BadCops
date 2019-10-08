a6dded3e871286878c2fdb7785b0ac74eff4c205
Gregor Mubi <gregor.mubi@outfit7.com>
2019-10-08 14:12:20 +0200

    asset importer helper... script to help with importing stuff...

---
+++ Assets/Code/AssetImporterHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AssetImporterHelper : MonoBehaviour {
+
+    [SerializeField] private bool DoIt = false;
+    [SerializeField] private GameObject[] objects;
+
+    private String path = "Assets/Resources/Prefabs/Tiles/";
+
+    void Update() {
+        if (DoIt) {
+            DoIt = false;
+            CreateDataFromOBJ();
+        }
+    }
+
+    //[MenuItem("Cool guy menu/Create data from .obj")]
+    private void CreateDataFromOBJ() {
+        foreach (GameObject obj in objects) {
+            for (int i = 0; i < obj.transform.childCount; i++) {
+                Transform child = obj.transform.GetChild(i);
+                child.position = new Vector3(1.5f, 0, 1.5f);
+            }
+            PrefabUtility.SaveAsPrefabAsset(obj, path + obj.name + ".prefab");
+        }
+    }
+
+
+}
