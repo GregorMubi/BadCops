d04eeb892673211d7435c295a7de44ef5df08973
Gregor Mubi <gregor.mubi@outfit7.com>
2019-10-11 09:57:28 +0200

    added World loader

---
+++ Assets/Code/Game/World/WorldLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldLoader : MonoBehaviour {
+
+    [SerializeField] private WorldData WorldData = null;
+    [SerializeField] private Transform TileParent = null;
+
+    //TODO move this to player/car controller
+    [SerializeField] private Transform PlayerPositionTransform = null;
+    [SerializeField] private Transform PlayerRotationTransform = null;
+
+    private List<TileController> LoadedTiles = new List<TileController>();
+
+    void Start() {
+        LoadWorld(WorldData);
+
+        PlayerPositionTransform.position = WorldData.SpawnPosition;
+        PlayerRotationTransform.eulerAngles = new Vector3(0, WorldData.SpawnRotation, 0);
+    }
+
+    void Update() {
+
+    }
+
+    public void LoadWorld(WorldData worldData) {
+        foreach (TileData tile in worldData.Tiles) {
+            TileController tileController = Instantiate(tile.TilePrefab);
+            tileController.transform.SetParent(TileParent);
+            tileController.transform.eulerAngles = new Vector3(0, tile.Rotation, 0);
+            tileController.transform.position = tile.Position;
+            LoadedTiles.Add(tileController);
+        }
+    }
+}
--- Assets/Code/WorldEditor/Data/WorldData.cs
+++ Assets/Code/WorldEditor/Data/WorldData.cs
@@ -2,8 +2,9 @@
 using UnityEngine;
 
 public class WorldData : ScriptableObject {
+    public Vector3 SpawnPosition = Vector3.zero;
+    public float SpawnRotation = 0f;
     public List<TileData> Tiles;
-
     public WorldData() {
         Tiles = new List<TileData>();
     }
