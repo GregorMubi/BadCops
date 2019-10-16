757356917e9649a28c0f8ef6806f2bade92c5c98
Gregor Mubi <gregor.mubi@outfit7.com>
2019-10-16 10:05:06 +0200

    added reverse gear to the car

--- Assets/Code/Game/SimpleCarController/SimpleCarController.cs
+++ Assets/Code/Game/SimpleCarController/SimpleCarController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -11,23 +12,14 @@ public class SimpleCarController : MonoBehaviour {
     [SerializeField] private float MaxSteeringAngle = 30f;
     [SerializeField] private float BreakForce = 1;
 
-    private float CurrentBrakeTorque = 0f;
-
     void Start() {
         RigidBody.centerOfMass += new Vector3(0, -0.2f, 0);
     }
 
     void Update() {
-        // BRAKE
-        if (Input.GetButton("Brake")) {
-            CurrentBrakeTorque = BreakForce;
-        } else {
-            CurrentBrakeTorque = 0f;
-        }
-
         // JUMP
         if (Input.GetButtonDown("Jump")) {
-            RigidBody.AddForce(RigidBody.centerOfMass + new Vector3(0f, 1000, 0f), ForceMode.Impulse);
+            RigidBody.AddForce(RigidBody.centerOfMass + new Vector3(0f, 100, 0f), ForceMode.Impulse);
         }
 
         // BOOST
@@ -40,6 +32,8 @@ public class SimpleCarController : MonoBehaviour {
         // INPUT
         float motor = MaxMotorTorque * Input.GetAxis("Accelerator");
         float steering = MaxSteeringAngle * Input.GetAxis("Horizontal");
+        double angle = AngleBetweenVectors(RigidBody.velocity, RigidBody.transform.forward);
+        bool isMovingForward = angle < 90 && angle > -90;
 
         foreach (SimpleAxleData axle in Axels) {
             if (axle.Steering) {
@@ -47,14 +41,46 @@ public class SimpleCarController : MonoBehaviour {
                 axle.RightWheel.steerAngle = steering;
             }
 
-            if (axle.Motor) {
-                axle.LeftWheel.motorTorque = motor;
-                axle.RightWheel.motorTorque = motor;
-            }
-
-            if (axle.Breaks) {
-                axle.LeftWheel.brakeTorque = CurrentBrakeTorque;
-                axle.RightWheel.brakeTorque = CurrentBrakeTorque;
+            if (isMovingForward) {
+                if (motor >= 0) {
+                    if (axle.Motor) {
+                        axle.LeftWheel.motorTorque = motor;
+                        axle.RightWheel.motorTorque = motor;
+                    }
+                    if (axle.Breaks) {
+                        axle.LeftWheel.brakeTorque = 0;
+                        axle.RightWheel.brakeTorque = 0;
+                    }
+                } else {
+                    if (axle.Breaks) {
+                        axle.LeftWheel.brakeTorque = BreakForce;
+                        axle.RightWheel.brakeTorque = BreakForce;
+                    }
+                    if (axle.Motor) {
+                        axle.LeftWheel.motorTorque = 0;
+                        axle.RightWheel.motorTorque = 0;
+                    }
+                }
+            } else {
+                if (motor > 0) {
+                    if (axle.Breaks) {
+                        axle.LeftWheel.brakeTorque = BreakForce;
+                        axle.RightWheel.brakeTorque = BreakForce;
+                    }
+                    if (axle.Motor) {
+                        axle.LeftWheel.motorTorque = 0;
+                        axle.RightWheel.motorTorque = 0;
+                    }
+                } else {
+                    if (axle.Motor) {
+                        axle.LeftWheel.motorTorque = motor * 0.5f;
+                        axle.RightWheel.motorTorque = motor * 0.5f;
+                    }
+                    if (axle.Breaks) {
+                        axle.LeftWheel.brakeTorque = 0;
+                        axle.RightWheel.brakeTorque = 0;
+                    }
+                }
             }
 
             ApplyLocalPositionToVisuals(axle.LeftWheel, axle.LeftWheelTransform);
@@ -73,6 +99,12 @@ public class SimpleCarController : MonoBehaviour {
         // APPLY POS/ROT
         wheelTransform.position = position;
         wheelTransform.rotation = rotation;
+    }
+
+    private double AngleBetweenVectors(Vector3 v1, Vector3 v2) {
+        double sin = v1.x * v2.z - v2.x * v1.z;
+        double cos = v1.x * v2.x + v1.z * v2.z;
+        return Math.Atan2(sin, cos) * Mathf.Rad2Deg;
     }
 
 }
