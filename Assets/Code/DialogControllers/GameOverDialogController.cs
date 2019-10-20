e4ab875ec03b9ce867c78be99ced626891de39b4
Tadej Mittoni <tadej.mittoni@outfit7.com>
2019-10-20 20:01:55 +0200

    Pause and game over dialogs.

---
+++ Assets/Code/DialogControllers/GameOverDialogController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverDialogController : MonoBehaviour
+{
+    [SerializeField]
+    public Text KillCountText;
+    [SerializeField]
+    public Text TimeText;
+
+    public void Restart() { }
+    public void Exit() { }
+    public void Init(int killCount, float timeInSeconds) {
+        KillCountText.text = killCount.ToString();
+        TimeText.text = timeInSeconds.ToString();
+    }
+}
---
+++ Assets/Code/DialogControllers/PauseDialogController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseDialogController : MonoBehaviour
+{
+    public void Resume() {
+
+    }
+
+    public void Exit() {
+
+    }
+}
