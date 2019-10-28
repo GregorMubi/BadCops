87e415506f28258cad8279104c6110e8b8b215c9
Rok Kos <rok.kos@outfit7.com>
2019-10-28 16:42:01 +0100

    VFX Smoke

---
+++ Assets/Code/Game/World/BuildingController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingController : MonoBehaviour
+{
+    [SerializeField] private Transform parentSmoke = null;
+    [SerializeField] private GameObject vfxSmokePrefab = null;
+
+    private bool isBurning = false;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Projectile" && !isBurning) {
+            GameObject smoke = Instantiate(vfxSmokePrefab, parentSmoke);
+            smoke.transform.localPosition = Vector3.zero;
+            Destroy(smoke, 10f);
+            isBurning = true;
+        }
+    }
+
+}
